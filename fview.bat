53f659860c95c5fd8660d0d522e9ab64985c69de
tz-dev <tillsbackupmail@gmx.de>
2025-07-06 10:57:28 +0200

    Update fview.bat
    - open file dialogue if no file has been specified

--- fview.bat
+++ fview.bat
@@ -1,11 +1,18 @@
 @echo off
-if "%~1"=="" (
-    echo Usage: fview ^<fits_file_path^>
+set FILE=%~1
+
+if "%FILE%"=="" (
+    echo No FITS file provided, opening file picker...
+    powershell -command "[System.Reflection.Assembly]::LoadWithPartialName('System.windows.forms') | Out-Null; $f=New-Object System.Windows.Forms.OpenFileDialog; $f.Filter='FITS files (*.fits)|*.fits'; if($f.ShowDialog() -eq 'OK'){write-host $f.FileName}" > tmp_file.txt
+    set /p FILE=<tmp_file.txt
+    del tmp_file.txt
+)
+
+if not exist "%FILE%" (
+    echo File not found: "%FILE%"
+    pause
     exit /b 1
 )
 
-python "fits_viewer.py" "%~1"
-if %ERRORLEVEL% neq 0 (
-    echo Error: Failed to run fits_browser.py
-    exit /b %ERRORLEVEL%
-)
+python "fits_viewer.py" "%FILE%"
+pause
